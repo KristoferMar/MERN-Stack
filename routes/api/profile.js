3fad0ce7302b5f30a94bd69d8f16224fd873b026
KristoferMar <kristofer@brostu.dk>
2020-03-15 02:02:35 +0100

    initial commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -125,10 +125,38 @@ router.get("/", async (req, res) => {
  * @Descrption   Get profile by user ID
  * @access       Public
  */
-router.get("/", async (req, res) => {
+router.get("/user/:user_id", async (req, res) => {
   try {
-    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
-    res.json(profiles);
+    const profile = await Profile.findOne({
+      user : req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+    if(!profile) {
+      return res.status(400).json({msg: 'Profile not found'});
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind === 'ObjectId'){
+      return res.status(400).json({msg: 'Profile not found'});
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+/**
+ * @route        DELETE api/profile
+ * @Descrption   Delete profile, user & posts
+ * @access       Private
+ */
+router.delete("/", auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+    // Removes profile
+    await Profile.findOneAndRemove({user: req.user_id});
+    // Removes user
+    await User.findOneAndRemove({_id: req.user_id});
+
+    res.json({ msg: 'User removed'});
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
