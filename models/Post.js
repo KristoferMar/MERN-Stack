12efc26d80889472cb86cdcca279417bf78eded4
KristoferMar <kristofer@brostu.dk>
2020-03-15 20:21:03 +0100

    Posts routing created

---
+++ models/Post.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+
+});
