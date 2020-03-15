e9cb75eff021006eb0b762e1a7c88967521eb043
KristoferMar <kristofer@brostu.dk>
2020-03-15 03:17:09 +0100

    created update / creation of experiences and education

---
+++ README.md
@@ -0,0 +1,3 @@
+# MERN Stack Application
+
+This application is a "Social media platform" for developers where you can make your own profile and extend it with varous information. It's created using Node.js, Express, MongoDB and React. 
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -163,4 +163,155 @@ router.delete("/", auth, async (req, res) => {
   }
 });
 
+
+/**
+ * @route        PUT api/profile
+ * @Descrption   Add profile experience
+ * @access       Private
+ */
+router.put('/experience', [auth, [
+  check('title', 'Title is required').not().isEmpty(),
+  check('company', 'Company is required').not().isEmpty(),
+  check('from', 'From date is required').not().isEmpty(),
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {
+    title,
+    company,
+    location,
+    from, 
+    to, 
+    current,
+    description
+  } = req.body;
+
+  const newExp = {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description
+  }
+
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    profile.experience.unshift(newExp);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
+
+/**
+ * @route        DELETE api/profile/experience/:exp_id
+ * @Descrption   Delete experience from profile
+ * @access       Private
+ */
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // GET remove index
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
+
+/**
+ * @route        PUT api/profile/education
+ * @Descrption   Add profile education
+ * @access       Private
+ */
+router.put('/education', [auth, [
+  check('school', 'school is required').not().isEmpty(),
+  check('degree', 'degree is required').not().isEmpty(),
+  check('from', 'From date is required').not().isEmpty(),
+  check('fieldofstudy', 'fieldofstudy date is required').not().isEmpty(),
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {
+    school,
+    degree,
+    fieldofstudy,
+    from, 
+    to, 
+    current,
+    description
+  } = req.body;
+
+  const newEdu = {
+    school,
+    degree,
+    fieldofstudy,
+    from, 
+    to, 
+    current,
+    description
+  }
+
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    profile.education.unshift(newEdu);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
+
+/**
+ * @route        DELETE api/profile/education/:edu_id
+ * @Descrption   Delete education from profile
+ * @access       Private
+ */
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // GET remove index
+    const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
+// Here we could implement Github repo implementation.
+
 module.exports = router;
