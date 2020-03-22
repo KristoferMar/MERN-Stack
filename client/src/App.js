db946d89c0369c8bf88ee2e7f5bdc26e23b77e46
KristoferMar <kristofer@brostu.dk>
2020-03-22 21:27:55 +0100

    Pots & Comments created and small issues , and features added

--- client/src/App.js
+++ client/src/App.js
@@ -2,57 +2,37 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Alert from './components/layout/Alert';
-import { loadUser } from "./actions/auth";
-import setAuthToken from "./utils/setAuthToken";
-import Dashboard from "./components/dashboard/Dashboard";
-import EditProfile from "./components/profile-forms/EditProfile";
-import PrivateRoute from "./components/routing/PrivateRoute";
-import CreateProfile from "./components/profile-forms/CreateProfile";
-import AddExperience from "./components/profile-forms/AddExperience";
-import AddEducation from "./components/profile-forms/AddEducation";
-import Profiles from "./components/profiles/Profiles";
+import Routes from './components/routing/Routes';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
-// Css
 import './App.css';
 
-if (localStorage.token) {
-  setAuthToken(localStorage.token) 
-}
-
-const App = () => { 
+const App = () => {
   useEffect(() => {
-    store.dispatch(loadUser());
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+      store.dispatch(loadUser());
+    }
   }, []);
 
   return (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing}></Route>
-        <section className="container">
-          <Alert />
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
           <Switch>
-            <Route exact path='/Register' component={Register}></Route>
-            <Route exact path='/Login' component={Login}></Route>
-            <Route exact path='/profiles' component={Profiles}></Route>
-            <PrivateRoute exact path='/dashboard' component={Dashboard}></PrivateRoute>
-            <PrivateRoute exact path='/create-profile' component={CreateProfile}></PrivateRoute>
-            <PrivateRoute exact path='/edit-profile' component={EditProfile}></PrivateRoute>
-            <PrivateRoute exact path='/add-experience' component={AddExperience}></PrivateRoute>
-            <PrivateRoute exact path='/add-education' component={AddEducation}></PrivateRoute>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
           </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-)};
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
